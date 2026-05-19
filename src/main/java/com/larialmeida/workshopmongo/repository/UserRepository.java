a1835c879fd7995df2b3a5e587c8acba2d052e74
Larissa Almeida <larih.a.rodrigues@gmail.com>
2026-05-19 14:19:09 -0300

    Conectando ao mongodb com repository e service

---
+++ src/main/java/com/larialmeida/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.larialmeida.workshopmongo.repository;
+
+import com.larialmeida.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
