59661e33bfaad47000d1a168cad6c4d9dfa995bd
Larissa Almeida <larih.a.rodrigues@gmail.com>
2026-07-03 17:22:03 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/larialmeida/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.larialmeida.workshopmongo.repository;
+
+import com.larialmeida.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
